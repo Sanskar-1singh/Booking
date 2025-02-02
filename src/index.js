3c9b0ad2d7e806c778c90b7e74bee0885a6e6968
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-02-02 10:43:35 +0530

    generated booking model

--- src/index.js
+++ src/index.js
@@ -9,4 +9,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+
 });
---
+++ src/utils/common/index.js
@@ -0,0 +1,6 @@
+module.exports={
+    ErrorResponse:require('./error-response'),
+    SuccessResponse:require('./success-response'),
+    ENUMS:require('./enum')
+}
+console.log(ENUM.BOOKING_STATUS);
